4c51fbb4a07e279b445df222dcc089c50380e18b
Lauren Barry <laurenbarry00@gmail.com>
2020-05-13 23:25:35 -0700

    Added getting comments from database

--- src/js/views/SortedCommentsView.js
+++ src/js/views/SortedCommentsView.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Session} from "bc-react-session";
 import CommentsList from "../components/CommentsList";
 
 var _ = require('lodash');
@@ -13,14 +14,8 @@ class SortedCommentsView extends Component {
   }
 
   componentDidMount() {
-    // TODO: remove dummy data json
+    /* TODO: remove dummy data json
     let dummyData = require("../../dummy_comments.json").comments;
-
-    /* TODO: setup comments fetching from API
-    fetch()
-      .then(response => this.setState({ comments: response.comments }));
-
-    */
 
     let sortedByPlatform = {};
     // generate a list of the available platforms with the comments for each platform
@@ -33,6 +28,31 @@ class SortedCommentsView extends Component {
     });
 
     this.setState({comments: sortedByPlatform})
+    */
+
+    const {payload} = Session.get();
+
+    fetch("http://127.0.0.1:8000/users/getcomments/",
+    {
+      method: "POST",
+      body: JSON.stringify({"username": payload.username, "password": payload.password}),
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(
+      (result) => {
+        let sortedByPlatform = {};
+        // generate a list of the available platforms with the comments for each platform
+        _.forEach(result.comments, function(comment) {
+          if (sortedByPlatform.hasOwnProperty(comment.platform)) {
+            sortedByPlatform[comment.platform].push(comment);
+          } else {
+            sortedByPlatform[comment.platform] = [comment];
+          }
+        });
+        this.setState({comments: sortedByPlatform})
+    });
   }
 
   render() {
